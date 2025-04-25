d89cbf25ae8fc42397fe9d0469b44e58c60cc5e9
Noha <nohaa.mdj@gmail.com>
2025-04-25 16:44:23 +0200

    replacement des fichiers sans dossier car bug

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
         />
         <Route path="/about" element={<About />} />
         <Route path="/apartment/:id" element={<ApartmentPage />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
 
